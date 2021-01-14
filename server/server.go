adf3eb7df8b977f2756fcf1eef74626149cf0eba
Nikhil Sreekumar <nikhs247@gmail.com>
2021-01-14 14:04:52 -0600

    image id

--- server/server.go
+++ server/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"image"
@@ -39,6 +40,63 @@ func performDetection(frame *gocv.Mat, results gocv.Mat) {
 			gocv.Rectangle(frame, image.Rect(left, top, right, bottom), color.RGBA{0, 255, 0, 0}, 2)
 		}
 	}
+}
+
+func (ts *TaskServer) TestPerformance(ctx context.Context, imgData *clientToTask.ImageData) (*clientToTask.ImageData, error) {
+	model := "data/frozen_inference_graph.pb"
+	config := "data/ssd_mobilenet_v1.pbtxt"
+	backend := gocv.NetBackendDefault
+	target := gocv.NetTargetCPU
+	// open DNN object tracking model
+	net := gocv.ReadNet(model, config)
+	if net.Empty() {
+		fmt.Printf("Error reading network model from : %v %v\n", model, config)
+		return nil, errors.New("Error reading network model")
+	}
+	defer net.Close()
+	net.SetPreferableBackend(gocv.NetBackendType(backend))
+	net.SetPreferableTarget(gocv.NetTargetType(target))
+
+	ratio := 1.0 / 127.5
+	mean := gocv.NewScalar(127.5, 127.5, 127.5, 0)
+	swapRGB := true
+
+	width := imgData.GetWidth()
+	height := imgData.GetHeight()
+	matType := imgData.GetMatType()
+	data := imgData.GetImage()
+
+	mat, err := gocv.NewMatFromBytes(int(width), int(height), gocv.MatType(matType), data)
+	if err != nil {
+		log.Fatalf("Error converting bytes to matrix: %v", err)
+	}
+
+	// convert image Mat to 300x300 blob that the object detector can analyze
+	blob := gocv.BlobFromImage(mat, ratio, image.Pt(300, 300), mean, swapRGB, false)
+
+	// feed the blob into the detector
+	net.SetInput(blob, "")
+
+	// run a forward pass thru the network
+	prob := net.Forward("")
+
+	performDetection(&mat, prob)
+
+	prob.Close()
+	blob.Close()
+
+	dims := mat.Size()
+	imgdata := mat.ToBytes()
+	mattype := int32(mat.Type())
+
+	retData := &clientToTask.ImageData{
+		Width:   int32(dims[0]),
+		Height:  int32(dims[1]),
+		MatType: mattype,
+		Image:   imgdata,
+	}
+
+	return retData, nil
 }
 
 func (ts *TaskServer) SendRecvImage(stream clientToTask.RpcClientToCargo_SendRecvImageServer) error {
